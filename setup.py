3d05e59938babc69d88da9bf8a64a8e07ae628c4
Vishnu Shaji <vishnushaji2602@gmail.com>
2024-08-04 21:29:28 +0530

    third commit

--- setup.py
+++ setup.py
@@ -1,4 +1,20 @@
 from setuptools import setup,find_packages 
+from typing import list
+
+HYPEN_E_DOT = '-e .'
+
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return the list of required libraries
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements
 
 setup( 
 	name='song_recommender', 
@@ -6,9 +22,5 @@ setup(
 	author='Vishnu Shaji', 
 	author_email='vishnushaji2602@gmail.com', 
 	packages=find_packages(), 
-	install_requires=[ 
-		'numpy', 
-		'pandas', 
-        'seaborn'
-	], 
+	install_requires=get_requirements('requirements.txt')
 ) 
